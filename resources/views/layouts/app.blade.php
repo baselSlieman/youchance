81e9787b35aca09354ea88fe87d5c198b97900b0
basel_slieman <bass889ha@gmail.com>
2025-04-23 14:09:01 +0300

    sixth update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,7 +103,7 @@
 	<title>@yield("title") | {{config('app.name')}}</title>
 
 	<!-- favicon -->
-	<link rel="shortcut icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
+	<link rel="shortcut icon" type="image/png" href="{{secure_asset('assets/img/favicon.png')}}">
 	<!-- google font -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
